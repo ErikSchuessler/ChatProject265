af997265099094b5f9a7b9a102142edf891b0da4
ErikSchuessler <schuesslererik@gmail.com>
2020-05-13 21:38:55 -0400

    Update Talk.py

--- Talk.py
+++ Talk.py
@@ -15,75 +15,39 @@ class Talk:
     '''
     
 
-    def __init__(self, ServerIP, ServerPort, OwnPort):
+    def __init__(self, ServerIP, ServerPort, OwnPort, eventHandler):
         self.serverIP=ServerIP
         self.serverPort=12001
         self.ownPort=12002
+        self.eventHandler = eventHandler
 
     def serverFunction(self):
         serverSocket=socket(AF_INET,SOCK_DGRAM)
         serverSocket.bind(('',self.ownPort))
-        #serverSocket.listen(1)
-        #connectionSocket,add=serverSocket.accept()
-
-        #print(str(add[0])+","+str(add[1])+" connects")
         while True:
             try:
                 msg=serverSocket.recvfrom(2048)
-                print("")
+                self.do_something(msg[0].decode().strip("'"))
                 print("[THEM:] " + msg[0].decode().strip("'"))
-                print("")
-                print("[PRESS 'Enter' TO BEGIN TYPING...]")
-
-                # Do not need to close connection as UDP is connectionless protocol
-
-                # if msg.upper()=="QUIT":
-                    # connectionSocket.close()
-                    # break
+                
             except Exception as e:
                 print("Server Error:")
                 print(e)
                 break
-        #connectionSocket.close()
+       
 
-    def clientFunction(self):
+
+    def clientFunction(self, msg2Send):
         clientSocket=socket(AF_INET,SOCK_DGRAM)
-        msg=input("[ME:]>")
+        #msg=input("[ME:]>")
         clientSocket.connect((self.serverIP, self.serverPort))
-        clientSocket.send(msg.encode())
-
-        #test change
-
-        #raise event here
-        do_something()
-
-        while True:
-            try:
-                msg=input("[ME:]>")
-                # print("[ME:]> "+msg) 
-                # print out the user input on the screen and overwrites the previous line
-                # print(CURSOR_UP_ONE+ERASE_LINE+"[ME:]> "+msg) 
-                clientSocket.send(msg.encode())
-
-                #raise event here
-
-                if msg.upper()=="QUIT":
-                    clientSocket.close()
-                    break
-            except Exception as e:
-                print("Client Error:")
-                print(e)
-                break
+        clientSocket.send(msg2Send.encode())
+        print("[ME:] "+msg2Send)
         clientSocket.close()
 
     def run(self):
         serverThread=threading.Thread(target=self.serverFunction)
-        clientThread=threading.Thread(target=self.clientFunction)
         serverThread.start()
-        clientThread.start()
-
-    
-    def do_something(self, update):
-           # do whatever I need to do
-           print("I've changed\n")
-           update()
+        
+    def do_something(self, message):
+           self.eventHandler(message)
